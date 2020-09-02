3cf946c88b4d45ef0384c79efac28c4bf7e2e012
Helton Isac <helton.isac@gmail.com>
2020-09-01 21:30:29 -0300

    Launching with Splash

--- app/src/main/java/br/com/hitg/pingpongx/SplashActivity.kt
+++ app/src/main/java/br/com/hitg/pingpongx/SplashActivity.kt
@@ -1,12 +1,20 @@
 package br.com.hitg.pingpongx
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val proximaTela = Intent(this, PlayerActivity::class.java)
+            startActivity(proximaTela)
+            finish()
+        }, 2000)
     }
 }
