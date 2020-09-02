73557ae4b3f46e3c4eb760bdd3939edcd291d4ef
Helton Isac <helton.isac@gmail.com>
2020-09-01 21:43:36 -0300

    Passing parameters by intent

--- app/src/main/java/br/com/hitg/pingpongx/MainActivity.kt
+++ app/src/main/java/br/com/hitg/pingpongx/MainActivity.kt
@@ -2,11 +2,19 @@ package br.com.hitg.pingpongx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupPlayers()
+    }
+
+    private fun setupPlayers() {
+        tvPlayerOneName.text = intent.getStringExtra("player1")
+        tvPlayerTwoName.text = intent.getStringExtra("player2")
     }
 }
--- app/src/main/java/br/com/hitg/pingpongx/PlayerActivity.kt
+++ app/src/main/java/br/com/hitg/pingpongx/PlayerActivity.kt
@@ -1,12 +1,24 @@
 package br.com.hitg.pingpongx
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_player.*
 
 class PlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player)
+
+
+        btStart.setOnClickListener {
+            val proximaTela = Intent(this, MainActivity::class.java)
+            proximaTela.putExtra("player1", etPlayer1.text.toString())
+            proximaTela.putExtra("player2", etPlayer2.text.toString())
+            startActivity(proximaTela)
+            finish()
+        }
+
     }
 }
